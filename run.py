67ee32129ed81277d4aa56d38fc928af330f472a
James King <jamesking.1711@gmail.com>
2024-05-13 09:45:48 +0000

    Added game rooms with corresponding keys.

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ def prompt():
     print("\t\t\tWelcome to the DnD Text Adventure\n"
         "Explore the Dungeon, collect items, fight enemies and take on the Dungeon Lord.\n\n"
         "Actions:\t'move {direction}' (moves you North, East, South or West)\n"
-        "\t\t'get {item}' (collect nearby items)\n\n")
+        "\t\t'get {item}' (collect nearby items)\n\n"
+        "\t\t'action {action}' (perform a corresponding action)\n\n")
 
     input("Press any key to continue...")
 
@@ -16,3 +17,18 @@ def clear():
     Clears screen
     """
     os.system('cls' if os.name == 'nt' else 'clear')
+
+# Dictionary containing all the possible rooms for the game
+# Each room is a key with directions corresponding to adjoining rooms
+rooms = {
+    'Lost Village' : {'North' : 'Haunted Mine', 'East' : 'Lumber Yard', 'South' : 'Howling Forest'}
+    'Haunted Mine' : {'East' :  'Flooded Mineshaft', 'South' : 'Lost Village'}
+    'Flooded Mineshaft' : {'West' : 'Haunted Mine', 'Item' : 'Holy Cross'}
+    'Howling Forest' : {'North' : 'Lost Village', 'East' : 'Wolf Den', 'South' : 'Clear Spring'}
+    'Wolf Den' : {'West' : 'Howling Forest', 'Item' : 'Faded Bible'}
+    'Clear Spring' : {'North' : 'Howling Forest', 'Action' : 'Heal'}
+    'Lumber Yard' : {'East' : 'Forbidden Grotto', 'West' : 'Lost Village'}
+    'Forbidden Grotto' : {'North' : 'Cemetary', 'South' : 'Whispering Crypt', 'West' : 'Lumber Yard'}
+    'Cemetary' : {'South' : 'Forbidden Grotto', 'Item' : 'Wooden Stake'}
+    'Whispering Crypt' : {'North' : 'Forbidden Grotto', 'Boss' : 'Vampire'}
+}
