8dbcf42827acf0eaaee0f4f28873dcb4fc3a6372
James King <jamesking.1711@gmail.com>
2024-05-13 09:19:45 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+def prompt():
+    """
+    Displays starting menu
+    """
+    print("\t\t\tWelcome to the DnD Text Adventure\n"
+        "Explore the Dungeon, collect items, fight enemies and take on the Dungeon Lord.\n\n"
+        "Actions:\t'move {direction}' (moves you North, East, South or West)\n"
+        "\t\t'get {item}' (collect nearby items)\n\n")
+
+    input("Press any key to continue...")
+
+def clear():
+    """
+    Clears screen
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
