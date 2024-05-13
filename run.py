60674c27e883d4f79b796ef8b32a25c57fd22373
James King <jamesking.1711@gmail.com>
2024-05-13 09:52:09 +0000

    Fixed bugs.

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def prompt():
     print("\t\t\tWelcome to the DnD Text Adventure\n"
         "Explore the Dungeon, collect items, fight enemies and take on the Dungeon Lord.\n\n"
         "Actions:\t'move {direction}' (moves you North, East, South or West)\n"
-        "\t\t'get {item}' (collect nearby items)\n\n"
+        "\t\t'get {item}' (collect nearby items)\n"
         "\t\t'action {action}' (perform a corresponding action)\n\n")
 
     input("Press any key to continue...")
@@ -21,16 +21,16 @@ def clear():
 # Dictionary containing all the possible rooms for the game
 # Each room is a key with directions corresponding to adjoining rooms
 rooms = {
-    'Lost Village' : {'North' : 'Haunted Mine', 'East' : 'Lumber Yard', 'South' : 'Howling Forest'}
-    'Haunted Mine' : {'East' :  'Flooded Mineshaft', 'South' : 'Lost Village'}
-    'Flooded Mineshaft' : {'West' : 'Haunted Mine', 'Item' : 'Holy Cross'}
-    'Howling Forest' : {'North' : 'Lost Village', 'East' : 'Wolf Den', 'South' : 'Clear Spring'}
-    'Wolf Den' : {'West' : 'Howling Forest', 'Item' : 'Faded Bible'}
-    'Clear Spring' : {'North' : 'Howling Forest', 'Action' : 'Heal'}
-    'Lumber Yard' : {'East' : 'Forbidden Grotto', 'West' : 'Lost Village'}
-    'Forbidden Grotto' : {'North' : 'Cemetary', 'South' : 'Whispering Crypt', 'West' : 'Lumber Yard'}
-    'Cemetary' : {'South' : 'Forbidden Grotto', 'Item' : 'Wooden Stake'}
-    'Whispering Crypt' : {'North' : 'Forbidden Grotto', 'Boss' : 'Vampire'}
+    'Lost Village' : {'North' : 'Haunted Mine', 'East' : 'Lumber Yard', 'South' : 'Howling Forest'},
+    'Haunted Mine' : {'East' :  'Flooded Mineshaft', 'South' : 'Lost Village'},
+    'Flooded Mineshaft' : {'West' : 'Haunted Mine', 'Item' : 'Holy Cross'},
+    'Howling Forest' : {'North' : 'Lost Village', 'East' : 'Wolf Den', 'South' : 'Clear Spring'},
+    'Wolf Den' : {'West' : 'Howling Forest', 'Item' : 'Faded Bible'},
+    'Clear Spring' : {'North' : 'Howling Forest', 'Action' : 'Heal'},
+    'Lumber Yard' : {'East' : 'Forbidden Grotto', 'West' : 'Lost Village'},
+    'Forbidden Grotto' : {'North' : 'Cemetary', 'South' : 'Whispering Crypt', 'West' : 'Lumber Yard'},
+    'Cemetary' : {'South' : 'Forbidden Grotto', 'Item' : 'Wooden Stake'},
+    'Whispering Crypt' : {'North' : 'Forbidden Grotto', 'Boss' : 'Vampire'},
 }
 
 # List to track inventory items collected by the player
@@ -46,4 +46,4 @@ current_room = "Lost Village"
 vowels = ['a', 'e', 'i', 'o', 'u']
 
 clear()
-promt()
+prompt()
